fb8ee45c996557ab28dc828e8ca61393338ffe8a
XMR13 <86971568+XMR13@users.noreply.github.com>
2023-03-22 10:46:24 +0700

    Update Basics.cpp

--- Basics.cpp
+++ Basics.cpp
@@ -3,5 +3,20 @@
 int main()
 {
   printf("mengetes saja cuy");
+  int haha = 100;
+  printf("berapa kali lah cara aku untuk mengetes hal ini %d \n", haha);
+  
+  
   return 0;
 }
+
+int haha(int iter)
+{
+  int angka{0};
+  for(int haha; haha<iter; iter++)
+  {
+    angka += haha; 
+  }
+  return angka;
+}
+
